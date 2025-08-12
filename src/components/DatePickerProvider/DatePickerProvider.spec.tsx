90a88ba3f424fec38bed9b6858d45325726cd3dd
Gean Lucas <gean.lfpsa@gmail.com>
2025-08-12 11:48:57 -0300

    test: update test configurations and add jest-dom for improved testing capabilities

--- src/components/DatePickerProvider/DatePickerProvider.spec.tsx
+++ src/components/DatePickerProvider/DatePickerProvider.spec.tsx
@@ -1,6 +1,16 @@
-import { cleanup } from '@testing-library/react'
-import { afterEach, describe } from 'vitest'
+import { cleanup, render } from '@testing-library/react'
+import { afterEach, describe, it } from 'vitest'
+import { DatePickerProvider } from '.'
+import { Calendar } from '../Calendar'
 
 describe('DatepickerProvider', () => {
   afterEach(cleanup)
+
+  it('should render calendar component', () => {
+    render(
+      <DatePickerProvider>
+        <Calendar />
+      </DatePickerProvider>
+    )
+  })
 })
